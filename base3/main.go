3cd3258676f0daaf63be2d563e7331c8cf2c6f26
dongchao <hdbysj@163.com>
2022-12-11 14:35:24 +0800

    map test

--- base3/main.go
+++ base3/main.go
@@ -7,6 +7,15 @@ import (
 )
 
 func main() {
+	// map study
+	var countryCapitalMap map[string]string
+	countryCapitalMap["1"] = "2"
+
+	for i, i2 := range countryCapitalMap {
+		fmt.Println(i)
+		fmt.Println(i2)
+	}
+
 	r := gee.New()
 	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
@@ -17,6 +26,6 @@ func main() {
 			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
 		}
 	})
-
 	r.Run(":9999")
+
 }
