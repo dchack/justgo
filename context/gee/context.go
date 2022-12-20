3b899aa2b4bee396e2d6dd31844bae3eb21ed70f
dongchao <hdbysj@163.com>
2022-12-20 18:21:24 +0800

    map test

--- context/gee/context.go
+++ context/gee/context.go
@@ -1,6 +1,10 @@
 package gee
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 // H interface 类似java 中的Object
 type H map[string]interface{}
@@ -25,4 +29,31 @@ func newContext(w http.ResponseWriter, r *http.Request) *Context {
 // PostForm Context的结构体方法
 func (c *Context) PostForm(key string) string {
 	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+func (c *Context) Status(code int) {
+	c.StateCode = code
+	c.Writer.WriteHeader(code)
+}
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+// interface{} 和 any
+func (c *Context) String(code int, format string, values ...any) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+
 }
